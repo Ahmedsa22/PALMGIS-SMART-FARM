b8c284c5340102ebab6ca3b684570c7d248dd14e
Ahmedsa22 <sabbarahmed22@gmail.com>
2026-08-29 12:21:59 +0100

    fix: import AllowAny dans core/views.py

--- Backend/backend/core/views.py
+++ Backend/backend/core/views.py
@@ -4,11 +4,12 @@ from rest_framework import viewsets, status
 from rest_framework.routers import DefaultRouter
 from core.models import User
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from core.permissions import IsManagerOrReadOnly
+
 
 
 @require_GET
